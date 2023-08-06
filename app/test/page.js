1d6bf932c971e6433eb0c41b2c6bf62d650fc785
Quochuynh <letrinhquoch@gmail.com>
2023-08-06 14:34:48 +0700

    added Tabs

--- app/test/page.js
+++ app/test/page.js
@@ -1,24 +1,43 @@
 'use client'
 import './styles.css';
 import AutoplayFullscreenPlayer from '@/components/AutoplayFullscreenPlayer/AutoplayFullscreenPlayer';
-import ProjectPreview from '@/components/ProjectPreview/ProjectPreview';
 import TestPreview from '@/components/TestPreview/TestPreview';
 import "react-modal-video/scss/modal-video.scss";
 import videos from '@/data/videos';
+import { Tabs, Tab, TabButtons } from '@/components/Tabs/Tabs';
 export default function Home() {
     return (
         <>
-            <AutoplayFullscreenPlayer url="https://player.vimeo.com/video/847725286" />
-            <button type='button' onClick={() => setOpen(true)}>Open</button>
-            <div className="grid grid-flow-row grid-cols-4 gap-12 px-72">
-                {/* <ProjectPreview url="https://player.vimeo.com/video/847725844" /> */}
-                {videos.map((video) => (
-                    <div className="col-span-2">
-                        <TestPreview video={video} />
-                    </div>
+            <section className='highlight-video'>
+                <AutoplayFullscreenPlayer url="https://player.vimeo.com/video/847725286" />
+            </section>
+            <section className="videos-preview">
+                <Tabs>
+                    <Tab label="Music Video">
+                        <div className="grid grid-flow-row grid-cols-4 gap-12 px-72">
+                            {/* <ProjectPreview url="https://player.vimeo.com/video/847725844" /> */}
+                            {videos.map((video) => (
+                                <div className="col-span-2">
+                                    <TestPreview video={video} />
+                                </div>
 
-                ))}
-            </div>
+                            ))}
+                        </div>
+                    </Tab>
+                    <Tab label="Fashion">
+                        <div className="grid grid-flow-row grid-cols-4 gap-12 px-72">
+                            {/* <ProjectPreview url="https://player.vimeo.com/video/847725844" /> */}
+                            {videos.map((video) => (
+                                <div className="col-span-2">
+                                    <TestPreview video={video} />
+                                </div>
+
+                            ))}
+                        </div>
+                        <span>Tab 2 content</span>
+                    </Tab>
+                </Tabs>
+            </section>
         </>
     )
 }
---
+++ components/Tabs/Tabs.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styles from './Tabs.module.css';
+export class Tabs extends React.Component {
+    state = {
+        activeTab: this.props.children[0].props.label
+    }
+    changeTab = (tab) => {
+
+        this.setState({ activeTab: tab });
+    };
+    render() {
+
+        let content;
+        let buttons = [];
+        return (
+            <div>
+                {React.Children.map(this.props.children, child => {
+                    buttons.push(child.props.label)
+                    if (child.props.label === this.state.activeTab) content = child.props.children
+                })}
+
+                <TabButtons activeTab={this.state.activeTab} buttons={buttons} changeTab={this.changeTab} />
+                <div className={styles.tab - content}>{content}</div>
+
+            </div>
+        );
+    }
+}
+
+export const TabButtons = ({ buttons, changeTab, activeTab }) => {
+
+    return (
+        <div className={styles.tabbuttons + " flex justify-center gap-4"}>
+            {buttons.map(button => {
+                return <button className={button === activeTab ? styles.button + ' ' + styles.active : styles.button} onClick={() => changeTab(button)}>{button}</button>
+            })}
+        </div>
+    )
+}
+
+export const Tab = props => {
+    return (
+        <React.Fragment>
+            {props.children}
+        </React.Fragment>
+    )
+}
