ba17561c81673b336b85e7ef3ba9c35ad1ad87f8
Quochuynh <letrinhquoch@gmail.com>
2023-09-04 00:07:04 +0700

    restyle video preview

--- components/AutoplayFullscreenPlayer/AutoplayFullscreenPlayer.js
+++ components/AutoplayFullscreenPlayer/AutoplayFullscreenPlayer.js
@@ -13,7 +13,7 @@ export default function AutoplayFullscreenPlayer({ url }) {
         }
     }, []);
     return (
-        <div className={`${classes.videoplayer} mx-72`}>
+        <div className={`${classes.videoplayer}`}>
             <iframe ref={iframeRef} src={url + "?title=0&byline=0&portrait=0&transparent=0&autoplay=1&muted=1&loop=1&autopause=0"} style={{ position: "absolute", top: "0", left: "0", width: "100%", height: "100%" }} frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen ></iframe>
             {/* <div class={classes.overlay}></div> */}
             {/* <video autoPlay muted loop>
